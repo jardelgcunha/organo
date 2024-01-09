1f98db5dfbcd8e614d316e16ed22b8221a0eb3d8
Jardel Cunha <jardelgcunha.ti@gmail.com>
2024-01-09 04:10:17 -0300

    Mapeando os Colaboradores
    - Integrando o componente Colaborador ao Time;
    - Integração dinâmica dos campos;
    - Desestruturação do props;
    - Estilização do TIme.

--- src/componentes/time/index.js
+++ src/componentes/time/index.js
@@ -2,16 +2,18 @@ import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = (props) => {
-  return (
-    <section
-      className='time'
-      style={{ backgroundColor: props.corSecundaria }}
-    >
-      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-      <Colaborador />
-      <Colaborador />
-    </section >
-  )
+    const css = { backgroundColor: props.corSecundaria }
+
+    return (
+        <section className='time' style={css}>
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador =>
+                    <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />
+                )}
+            </div>
+        </section >
+    )
 }
 
 export default Time
