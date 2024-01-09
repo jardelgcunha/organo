074299b057deadf109dc5c557fb388a56a2bf24f
Jardel Cunha <jardelgcunha.ti@gmail.com>
2024-01-09 06:16:53 -0300

    Teste Pasta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
-import Time from './componentes/Time';
+import Time from './componentes/TIMEteste';
 import Footer from './componentes/Footer';
 
 function App() {
